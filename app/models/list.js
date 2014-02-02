6f456ecb402f3daadf810be5c1ae72a49ed76387
Timothy Kendall <timkendall18@gmail.com>
2014-02-01 17:39:27 -0800

    Created List model.

---
+++ app/models/list.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * List Schema
+ */
+var ListSchema = new Schema({
+    name: String,
+    creator: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    public: {
+        type: Boolean,
+        default: false
+    },
+    count : {
+        type: Number,
+        default: 0
+    },
+    items: [{
+        type: Schema.ObjectId,
+        ref: 'Link'
+    }]
+});
+
+/**
+ * Virtuals
+ */
+
+
+/**
+ * Validations
+ */
+var validatePresenceOf = function(value) {
+    return value && value.length;
+};
+
+/**
+ * Pre-save hook
+ */
+ListSchema.pre('save', function(next) {
+    if (!this.isNew) return next();
+
+    if (!validatePresenceOf(this.name))
+        next(new Error('Missing list name'));
+    else
+        next();
+});
+
+/**
+ * Methods
+ */
+ListSchema.methods = {
+    /**
+     * findSimilarTypes - return lists with similar names; for fun
+     *
+     * @param {Object} cb
+     * @return {Array}
+     * @api public
+     */
+    findSimilarTypes: function(cb) {
+        return this.model('List').find({ name: this.name }, cb);
+    }
+
+};
+
+mongoose.model('List', ListSchema);
