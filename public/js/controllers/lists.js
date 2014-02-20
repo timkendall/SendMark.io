ac1227a21ed38e5290f49a0fec05218b934e40dc
Timothy Kendall <timkendall18@gmail.com>
2014-02-20 09:20:57 -0800

    Added Socket.io npm  dependency.

--- public/js/controllers/lists.js
+++ public/js/controllers/lists.js
@@ -70,7 +70,6 @@ angular.module('SendMark.lists').controller('ListsCtrl', ['$scope', '$routeParam
 			listId: $routeParams.listId
 		}, function(list) {
 			$scope.list = list;
-			console.log("Current list items: " + $scope.list._items);
 		});
 	};
 }]);
