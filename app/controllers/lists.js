2d7f27ed058d59e12413fa0fd02de5950fc24ff3
Timothy Kendall <timkendall18@gmail.com>
2014-03-19 09:32:22 -0700

    Fixed controllers/lists.js, removed old and unnecesary code.

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -4,118 +4,101 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-		List = mongoose.model('List'),
-		User = mongoose.model('User'),
-		_ = require('lodash');
+	List = mongoose.model('List'),
+	User = mongoose.model('User'),
+	_ = require('lodash');
 
 /**
  * Load all of a users lists
  */
-exports.all = function(req, res, next) {
-		console.log("loading users lists");
+exports.all = function (req, res, next) {
+	console.log("loading users lists");
 
-		List.loadAllOfUsers(req.user, function(err, lists) {
-				if (err) return next(err);
-				if (!lists) return next(new Error('Failed to load lists.'));
+	List.loadAllOfUsers(req.user, function(err, lists) {
+		if (err) return next(err);
+		if (!lists) return next(new Error('Failed to load lists.'));
 
-				res.jsonp(lists);
-		});
-		/*
-		res.jsonp([
-				{ name: "A list" },
-				{ name: "Another list" },
-				{ name: "Last list" }
-		]);
-		*/
+		res.jsonp(lists);
+	});
 };
 
 /**
  * Find list by id
  */
-exports.list = function(req, res, next, id) {
-		List.load(id, function(err, list) {
-				if (err) return next(err);
-				if (!list) return next(new Error('Failed to load list ' + id));
-				req.list = list;
-				next();
-		});
+exports.list = function (req, res, next, id) {
+	List.load(id, function(err, list) {
+		if (err) return next(err);
+		if (!list) return next(new Error('Failed to load list ' + id));
+		req.list = list;
+		next();
+	});
 };
 
 /**
  * Create a list
  */
 exports.create = function(req, res) {
-		var list = new List(req.body);
-		list.creatorUsername = req.user.username;
+	var list = new List(req.body);
+	list._creator = req.user.id;
 
-		// A pain but load user with id to get _id
-		User.load(req.user.id, function (error, user) {
-				list._creator = user._id;
-
-				// Correct way to access UserApp user
-				console.log('Creating list for ' + req.user.username + ', with user_id: ' + req.user.id);
-
-
-				list.save(function(err) {
-						if (err) {
-								return res.send('users/signup', {
-										errors: err.errors,
-										list: list
-								});
-						} else {
-								res.jsonp(list);
-						}
-				});
+	list.save(function (err) {
+		if (err) {
+			return res.send('users/signup', {
+				errors: err.errors,
+				list: list
+			});
+		} else {
+				res.jsonp(list);
+			}
 		});
-
 };
 
 /**
  * Update a list
  */
 exports.update = function(req, res) {
-		var list = req.list;
+	var list = req.list;
 
-		list = _.extend(list, req.body);
+	list = _.extend(list, req.body);
 
-		list.save(function(err) {
-				if (err) {
-						return res.send('users/signup', {
-								errors: err.errors,
-								list: list
-						});
-				} else {
-						res.jsonp(list);
-				}
-		});
+	list.save(function(err) {
+		if (err) {
+			return res.send('users/signup', {
+				errors: err.errors,
+				list: list
+			});
+		} else {
+			res.jsonp(list);
+		}
+	});
 };
 
 /**
  * Delete a list
  */
 exports.destroy = function(req, res) {
-		var list = req.list;
+	var list = req.list;
 
-		// Don't allow deletion of 'Uncategorized'
-		if (list.name === 'Uncategorized') return res.jsonp({ message: 'Can\'t delete list "Uncategorized!"' });
+	// Don't allow deletion of 'Uncategorized'
+	if (list.name === 'Uncategorized') return res.jsonp({ message: 'Can\'t delete list "Uncategorized!"' });
 
-		// Otherwise remove list
-		list.remove(function(err) {
-				if (err) {
-						return res.send('users/signup', {
-								errors: err.errors,
-								list: list
-						});
-				} else {
-						res.jsonp(list);
-				}
-		});
+	// Otherwise remove list
+	list.remove(function(err) {
+		if (err) {
+			return res.send('users/signup', {
+				errors: err.errors,
+				list: list
+			});
+		} else {
+			res.jsonp(list);
+		}
+	});
 };
 
 /**
  * Show a list
  */
 exports.show = function(req, res) {
-		res.jsonp(req.list);
+	res.jsonp(req.list);
 };
 
